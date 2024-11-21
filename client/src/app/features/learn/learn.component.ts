7c427bc6efc9a1a3baddaeb69e963eab2b988a5a
tonileo <mdtonileo@gmail.com>
2024-11-21 22:36:28 +0100

    Implemented design for learn component

--- client/src/app/features/learn/learn.component.ts
+++ client/src/app/features/learn/learn.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-learn',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './learn.component.html',
   styleUrl: './learn.component.scss'
 })
 export class LearnComponent {
+  public answearClicked: boolean = false;
 
+  showAnswear(){
+    this.answearClicked = !this.answearClicked;
+  }
 }
